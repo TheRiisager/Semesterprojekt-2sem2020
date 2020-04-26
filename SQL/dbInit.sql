518d1001d9391cd83617f5be574f18a42ed9bc21
youneselb <youneselb@gmail.com>
2020-04-26 14:14:01 +0200

    sql test

--- SQL/dbInit.sql
+++ SQL/dbInit.sql
@@ -1,26 +1,9 @@
--- noinspection SqlDialectInspectionForFile
-
--- noinspection SqlNoDataSourceInspectionForFile
-
-CREATE DATABASE  IF NOT EXISTS `useradmin`;
-
-
-USE `useradmin`;
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(90) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `role` varchar(20) NOT NULL DEFAULT 'customer',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES 
-(1,'jens@somewhere.com','jensen','customer'),
-(2,'ken@somewhere.com','kensen','customer'),
-(3,'robin@somewhere.com','batman','employee');
-UNLOCK TABLES;
+DROP TABLE IF EXISTS ordersmaterials;
+CREATE TABLE ordersmaterials
+(
+	materialsorderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quantity INT NOT NULL,
+	FOREIGN KEY (orderID) REFERENCES Orders(orderID),
+    FOREIGN KEY (materialID) REFERENCES Material(materialID)
+);
 
