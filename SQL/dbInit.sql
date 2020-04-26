7224782df482e9fe8b92c43af2775c437c864bc1
youneselb <youneselb@gmail.com>
2020-04-26 14:46:30 +0200

    sql tabeller

--- SQL/dbInit.sql
+++ SQL/dbInit.sql
@@ -1,9 +1,20 @@
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders
+(
+	orderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    length INT NOT NULL,
+    width INT NOT NULL, 
+    userID INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
 DROP TABLE IF EXISTS ordersmaterials;
 CREATE TABLE ordersmaterials
 (
 	materialsorderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     quantity INT NOT NULL,
+    orderID INT NOT NULL, 
+    materialID INT NOT NULL,
 	FOREIGN KEY (orderID) REFERENCES Orders(orderID),
     FOREIGN KEY (materialID) REFERENCES Material(materialID)
 );
-
