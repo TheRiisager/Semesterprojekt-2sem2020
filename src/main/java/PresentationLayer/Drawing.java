a7e3f4679a6391f119fba1189b5c660372b967dc
Frederik Johnsen <frederik@familienjohnsen.dkgit config --global user.email frederik@familienjohnsen.dk>
2020-05-04 12:18:11 +0200

    fixed Drawing.java

--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -1,21 +1,20 @@
 package PresentationLayer;
 
-import FuntionLayer.LoginSampleException;
-import FunktionLayer.Svg;
+import FunctionLayer.LoginSampleException;
+import FunctionLayer.Svg;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSerevletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 public class Drawing extends Command {
 
-    @
-
-String execute(HTTPServletRequest, request, HTTPServletResponse, response) throws LoginSampleException)
-    Svg svg = new Svg(800,600), "0,0,800,600", 0,0,);
-    svg.addRect(0,0,600,780);
-    svg.addRect(0,35,4,780);
-    svg.addRect(0,565,4,780);
-    request.setAttribute("svgdrawing"), svg.toString());
-    return "drawing";
-}
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+        Svg svg = new Svg( 800,600, "0,0,800,600", 0,0 );
+        svg.addRect(0,0,600,780);
+        svg.addRect(0,35,4,780);
+        svg.addRect(0,565,4,780);
+        request.setAttribute("svgdrawing", svg.toString() );
+        return "drawing";
+    }
 }
